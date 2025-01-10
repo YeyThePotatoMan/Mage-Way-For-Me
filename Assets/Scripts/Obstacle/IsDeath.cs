c979e2ef7b4d0335a36a37bed149eb60f16b90c7
Shui <faiq.azzam@gmail.com>
2025-01-10 19:22:38 +0700

    Edited isDeath script
    Removed the debug function and add a little bit of comments

--- Assets/Scripts/Obstacle/IsDeath.cs
+++ Assets/Scripts/Obstacle/IsDeath.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class IsDeath : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnCollisionEnter2D(Collision2D other) // When player hit the obstacle
     {
         var player = other.collider.GetComponent<PlayerMovement>();
         if (player != null)
         {
-            Debug.Log("death");
+            // Debug.Log("death");
             player.Die();
         }
     }
